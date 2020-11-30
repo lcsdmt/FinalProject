d09b2656ad9f59aa84d1400cdd2215e18e27603d
lcsdmt <lcsdmt@users.noreply.github.com>
2020-11-30 15:07:07 -0600

    11.30

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './App';
+import App from './Home';
 import './scss/app';
 
 render(<App />, document.getElementById("root"));
