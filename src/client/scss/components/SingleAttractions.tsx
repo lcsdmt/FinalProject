8e5bd2b86c766290646b500c819232fb1d2f2f07
Bullis61 <isbellhunt@gmail.com>
2020-12-03 14:47:17 -0600

    updated attractions pages again

--- src/client/scss/components/SingleAttractions.tsx
+++ src/client/scss/components/SingleAttractions.tsx
@@ -4,11 +4,13 @@ import { iAttractions } from "../../utils/types";
 
 const SingleAttractions: React.FC = (props) => {
   const [attraction, setAttraction] = useState<iAttractions>({});
+  const [hours, setHours] = useState<Array<any>>([]);
+  const [ifOpen, setIfOpen] = useState({});
     // console.log(props.hey);
+
 
   const fetchAttractions = async () => {
     try {
-      console.log("test");
       const url = "https://cors-anywhere.herokuapp.com/";
       await fetch(
         url +
@@ -18,6 +20,8 @@ const SingleAttractions: React.FC = (props) => {
         .then((data) => {
             if(data.result) {
                 setAttraction(data.result)
+                setHours(data.result.opening_hours.weekday_text);
+                setIfOpen(data.result.opening_hours.open_now);
             } else {
                 return
             }
@@ -33,12 +37,28 @@ const SingleAttractions: React.FC = (props) => {
   }, []);
 
   return (
-    <div key={attraction.place_id}>
-      <h5>{attraction.name}</h5>
-      <h5>{attraction.formatted_phone_number}</h5>
-      <h5>{attraction.formatted_address}</h5>
-      <h5>{attraction.url}</h5>
-    </div>
+    <>
+      <div className="card">
+        <div className="card-body shadow">
+          {/* <h5>{attraction.photos}</h5> */}
+          <h5>
+            <u>{attraction.name}</u>
+          </h5>
+          <h5 className="text-info">{attraction.formatted_phone_number}</h5>
+          <h5>{attraction.formatted_address}</h5>
+          <a href={attraction.website}>{attraction.website}</a>
+          <br />
+          <a href={attraction.url}>{attraction.url}</a>
+          <h5>Google User Rating:{attraction.rating}</h5>
+          {/* <p>{attraction.description}</p> */}
+
+          {hours.map((hour) => (
+            <h6>{hour}</h6>
+          ))}
+          {/* <h5>{attraction.opening_hours}</h5> */}
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/client/utils/types.ts
+++ src/client/utils/types.ts
@@ -1,38 +1,47 @@
 export interface iRestaurants {
-    name: string;
-    url: string;
-    formatted_phone_number: string;
-    formatted_address: string;
-    opening_hours: string;
-    weekday_text: [];
-    business_status: string;
-    vicinity: string;
-    photos: [];
-    place_id: string;
+  name: string;
+  url: string;
+  website: string;
+  formatted_phone_number: string;
+  formatted_address: string;
+  opening_hours: {weekday_text: Array<any>};
+  description: string;
+  weekday_text: string;
+  rating: number;
+  business_status: string;
+  vicinity: string;
+  photos?: string;
+  place_id: string;
   }
 
   export interface iBars {
-      name: string;
-      url: string;
-      formatted_phone_number: string;
-      formatted_address: string;
-      opening_hours: string;
-      weekday_text: [];
-      business_status: string;
-      vicinity: string;
-      photos: [];
-      place_id: string;
+    name: string;
+    url: string;
+    website: string;
+    formatted_phone_number: string;
+    formatted_address: string;
+    opening_hours: {weekday_text: Array<any>};
+    description: string;
+    weekday_text: string;
+    rating: number;
+    business_status: string;
+    vicinity: string;
+    photos?: string;
+    place_id: string;
     }
 
     export interface iAttractions {
-        name: string;
-        url: string;
-        formatted_phone_number: string;
-        formatted_address: string;
-        opening_hours: string;
-        weekday_text: [];
-        business_status: string;
-        vicinity: string;
-        photos: [];
-        place_id: string;
+      name: string;
+      url: string;
+      website: string;
+      formatted_phone_number: string;
+      formatted_address: string;
+      opening_hours: {weekday_text: Array<any>};
+      description: string;
+      weekday_text: string;
+      rating: number;
+      business_status: string;
+      vicinity: string;
+      photos?: string;
+      place_id: string;
       }
