46e4d786548f20549cfdf027240865066fd04827
lcsdmt <lcsdmt@users.noreply.github.com>
2020-12-03 16:53:23 -0600

    small changes

--- src/client/components/singlerestaurant.tsx
+++ src/client/components/singlerestaurant.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { iRestaurants } from "../utils/types";
-import { Link } from "react-router-dom";
-import restaurantsDB from "../../server/db/restaurantsDB";
+
 
 const SingleRestaurant: React.FC = (props) => {
   const [restaurant, setRestaurant] = useState<iRestaurants>({});
