c81a74887fbb40a72208ab77e2f60b4ca09da3ef
Ashley Webster <ashwebster74@gmail.com>
2020-12-04 13:31:54 -0600

    updates in auth routes

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,28 +1,20 @@
 import * as express from 'express';
 import * as path from 'path';
 import * as passport from 'passport';
-
-
-import apiRouter from './routes';
-
-
+import './middleware/localstrategy';
+import './middleware/bearerstrategy';
+import routes from './routes';
 
 const app = express();
-
 
 app.use(express.json());
 app.use(passport.initialize());
 
-app.use(apiRouter);
-app.use(express.static('public'))
-
+app.use(routes);
+app.use(express.static('public'));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'))
 });
-;
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port ${port}...`));
-
