51b258e638a326fac1d66ad84401ca9d7feb2d69
wwperry94 <wwperry94@gmail.com>
2020-12-10 10:54:13 -0600

    final merge

--- src/client/components/navBar.tsx
+++ src/client/components/navBar.tsx
@@ -14,7 +14,13 @@ const NavBar: React.FC = (props: NavBarProps) => {
     }
   }
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark"
+    style={{
+      position: "sticky",
+      top: 0,
+      zIndex: 100
+    }}
+    >
       <a className="navbar-brand" href="/"><strong>Local Host</strong></a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
